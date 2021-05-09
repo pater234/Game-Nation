18e5e6fb82af4516266c9bb9982fd6435e467d51
Rishthegreat <rishitarora2005@gmail.com>
2021-05-09 17:34:12 -0400

    Adding changes to Learning.php and updating it with some php code

--- LearningExperimentingPHP/Learning1.php
+++ LearningExperimentingPHP/Learning1.php
@@ -1,7 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello</title>
+    </head>
+    <body>
+    <?php
+        echo("Hello World"); // echo writes out html (You can write html code)
+        echo ("<h1>Welcome to the website</h1> <hr>");
+        // variables in php
+        $myInt = 45;
+        $myBool = true;
+        $myString = "Hello";
+        $myString = strtoupper($myString);
+        $numOfChars = strlen($myString);
+        if ($myBool) {
+            echo ("<h3>This variable is true. variable name is $myString</h3>");
+        } else {
+            echo ("<h3>This variable is false. variable name is $myString</h3>");
+        }
+        echo($myString[0]);
 
-
-class Learning1
-{
-
-}
+    ?>
+    </body>
+</html>
