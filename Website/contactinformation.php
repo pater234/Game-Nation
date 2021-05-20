6fba340b780cdaeee30ca2fe1e5489edab0252e4
Rishthegreat <rishitarora2005@gmail.com>
2021-05-19 20:36:37 -0400

    Adding some changes

--- Website/contactinformation.php
+++ Website/contactinformation.php
@@ -1,6 +1,15 @@
 <?php include ($_SERVER["DOCUMENT_ROOT"] . "/layout.php"); ?>
 
 
-<a>This is contact Information</a>
+<h1>Contact us by email: </h1>
+<ul>
+    <li>Main business email - <a href="mailto:classes56official@gmail.com">classes56official@gmail.com</a></li>
+</ul>
 
+<h1>Contact by social media</h1>
+<ul>
+    <li>Facebook - </li>
+    <li>Instagram - </li>
+    <li>YouTube <a href="https://www.youtube.com/channel/UCot7W_-D9sqeQoj3cMOYBdw">Classes56 Channel</a></li>
+</ul>
 <?php include ($_SERVER["DOCUMENT_ROOT"] . "/footer.php"); ?>
