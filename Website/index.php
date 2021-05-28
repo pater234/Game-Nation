82b357a9c6eef573dcf3c025d8b0f910fa7072c2
Rishthegreat <rishitarora2005@gmail.com>
2021-05-28 12:34:00 -0400

    Adding some changes

--- Website/index.php
+++ Website/index.php
@@ -1,2 +1,19 @@
 <?php include ($_SERVER["DOCUMENT_ROOT"] . "/layout.php"); ?>
+
+
+<style>
+    .body {
+        font-family: "Montserrat", sans-serif;
+    }
+</style>
+
+<body class="body">
+    <div style="background: linear-gradient(-45deg, #ddecff 50%, #ffffff 50%)">
+        <h1 style="text-align: center; padding-top: 50px; padding-bottom: 50px; font-size: 50px; text-decoration: underline; text-decoration-color: #007bff;">Welcome to Classes56!</h1>
+    </div>
+
+    <h2>What is our Mission?</h2>
+
+</body>
+
 <?php include ($_SERVER["DOCUMENT_ROOT"] . "/footer.php"); ?>
